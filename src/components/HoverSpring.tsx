038ffc1e40ad351d70953578da12d2cb21cfddfd
amjido-01 <youndsadeeq10@gmail.com>
2024-08-06 21:17:04 +0100

    auth

--- src/components/HoverSpring.tsx
+++ src/components/HoverSpring.tsx
@@ -3,47 +3,58 @@ import { motion } from 'framer-motion'
 import Image from 'next/image'
 import { BookOpen } from 'lucide-react'
 
+interface ProjectsData {
+  id: number;
+  name: string;
+  description: string;
+  link: string;
+  icon: JSX.Element;
+}
+
+const iconSize = 18;
+
 const ProjectsData = [
   {
     id: 1,
     name: 'Expense Tracking',
     description: 'Easily add and categorize your expenses.',
     link: 'https://syntaxui.com',
-    image: BookOpen,
+    icon: <BookOpen size={iconSize} />,
   },
   {
     id: 2,
     name: 'Budget Management',
     description: 'Set and track your budgets with ease.',
     link: 'https://prettyfolio.com',
-    image: BookOpen,
+    icon: BookOpen,
   },
   {
     id: 2,
     name: 'Reports and Analytics',
     description: 'Gain insights with detailed reports and visual charts.',
     link: 'https://enchant.ansubkhan.com',
-    image: BookOpen,
+    icon: BookOpen,
   },
   {
     id: 3,
     name: 'Secure Authentication',
     description: 'Securely log in with OAuth providers like Google.',
     link: 'https://ansubkhan.com',
-    image: BookOpen,
+    icon: BookOpen,
   },
   {
     id: 4,
     name: 'Reports and Analytics',
     description: 'Gain insights with detailed reports and visual charts.',
     link: 'https://enchant.ansubkhan.com',
-    image: BookOpen,
+    icon: <BookOpen />,
   },
 ]
 
 const HoverSpring = () => {
   return (
     <div className='container border-2 mt-20'>
+      <h2 className='scroll-m-20 text-3xl text-center font-extrabold tracking-tight lg:text-4xl'>Features</h2>
       <div className="container grid w-full grid-cols-2 gap-x-10 md:grid-cols-3">
         {ProjectsData.map((project) => {
           return (
@@ -60,7 +71,7 @@ const HoverSpring = () => {
             >
               <a target="_blank" rel="noopener noreferrer" href={project.link}>
                 <Image
-                  src={project.image}
+                  src={project.icon}
                   width={30}
                   height={30}
                   className="mb-3 rounded-lg border-gray-400 dark:border"
